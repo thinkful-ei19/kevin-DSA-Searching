2ce08c07ab8a21b8f658d21389a8a8f195ab69eb
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-23 13:44:04 -0700

    find book

--- drills/drills.js
+++ drills/drills.js
@@ -1,6 +1,8 @@
 'use strict';
 
-library = [
+// DEWEY DECIMAL CHALLENGE
+
+const library = [
   { author: 'Cowlishaw, Mike', dewey: '005.133', title: 'The REXX Language' },
   { author: 'Sams', dewey: '005.133', title: 'Teach Yourself C++ In 21 Days' },
   { author: 'Stroustrup., Bjarne', dewey: '005.133', title: 'The C++ Programming Language' },
@@ -13,8 +15,20 @@ library = [
   { author: 'Norris, Chuck', dewey: '796.8092', title: 'The Official Chuck Norris Fact Book' }
 ];
 
-function findBook(deweyNum) {
-
+function findBook(book) {
+  for (let i = 0; i < library.length; i++) {
+    if (library[i].author === book.author && library[i].dewey === book.dewey && library[i].title === book.title) {
+      return 'We have the book!'
+    }
+  };
+  return 'We don\'t have the book!'
 }
 
-console.log(findBook());
+const book = { author: 'Schmidt, Meinhard', dewey: '005.44684', title: 'Windows Vista for Dummies' }
+console.log(findBook(book));
+
+
+// TREE TRAVERSAL
+
+
+
